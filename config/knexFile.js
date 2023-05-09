9c49f6d240e5a2692a35bd5968076908c1dc52a5
Valentina-Peralta <125395224+Valentina-Peralta@users.noreply.github.com>
2023-05-09 08:59:45 -0300

    conexion con elephant solucionada

--- config/knexFile.js
+++ config/knexFile.js
@@ -1,31 +1,15 @@
-/* const knex = require('knex')({
-    client: 'pg',
-    connection: {
-        //conString: 'postgres://lltktrdq:bz_B7nx31nAAPYN9_yGs3HGjk3N3wYRe@tuffi.db.elephantsql.com/lltktrdq',
-
-        host: 'tuffi.db.elephantsql.com',
-        port: 5432,
-        user: 'lltktrdq',
-        password: process.env.PASSWORD,
-        database: 'proyecto-final-senpai',
-        ssl: {
-            require: true, // This will help you. But you will see nwe error
-            rejectUnauthorized: false // This line will fix new error
-        }
-    }
-})
-module.exports = knex
- */
 
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        host: 'localhost',
-        port: 5432,
-        user: 'postgres',
-        password: 'Vperalta',
-        database: 'proyecto-final-senpai',
+
+        host: 'tuffi.db.elephantsql.com',
+        user: 'lltktrdq',
+        password: process.env.PASSWORD,
+        database: 'lltktrdq',
 
     }
 })
 module.exports = knex
+
+
