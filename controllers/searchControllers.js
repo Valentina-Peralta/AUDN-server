64c7123ceb48cf3f19548a530bc368944f0d8cc5
Valentina-Peralta <125395224+Valentina-Peralta@users.noreply.github.com>
2023-05-15 01:36:04 -0300

    controller search songs by genre

--- controllers/searchControllers.js
+++ controllers/searchControllers.js
@@ -6,13 +6,13 @@ exports.searchSongs = async (req, res) => {
         const searchTerm = req.body.searchTerm?.trim();
 
         const songs = await knex('songs')
-            .select('songs.name as song_name', 'albums.image as album_image', 'albums.name as album_name', 'artists.name as artist_name')
+            .select('songs.name as song_name', 'songs.id as song_id', 'albums.image as album_image', 'albums.name as album_name', 'artists.name as artist_name')
             .join('albums', 'songs.album_id', 'albums.id')
             .join('artists', 'albums.artist_id', 'artists.id')
             .whereRaw(`LOWER(songs.name) LIKE ?`, `%${searchTerm.toLowerCase()}%`);
 
         const albums = await knex('songs')
-            .select('songs.name as song_name', 'albums.image as album_image', 'albums.name as album_name', 'artists.name as artist_name')
+            .select('songs.name as song_name', 'songs.id as song_id', 'albums.image as album_image', 'albums.name as album_name', 'artists.name as artist_name')
             .join('albums', 'songs.album_id', 'albums.id')
             .join('artists', 'albums.artist_id', 'artists.id')
             .whereRaw(`LOWER(artists.name) LIKE ?`, `%${searchTerm.toLowerCase()}%`);
